b51d30676fce863b08614831a454e41d291c2399
Naveen Kumar Matturthi <RSNKumar@users.noreply.github.com>
2019-12-10 21:18:19 +0530

    First version

--- docs/index.js
+++ docs/index.js
@@ -1,126 +1,89 @@
-var barCount = 60;
-var initialDateStr = '01 Apr 2017 00:00 Z';
+$(function(){
+	var barCount = 60;
+	var initialDateStr = '01 Apr 2017 00:00 Z';
+	formattedData = [];
 
-var ctx = document.getElementById('chart').getContext('2d');
-ctx.canvas.width = 1000;
-ctx.canvas.height = 250;
-var chart = new Chart(ctx, {
-	type: 'candlestick',
-	data: {
-		datasets: [{
-			label: 'CHRT - Chart.js Corporation',
-			data: getRandomData(initialDateStr, barCount)
-		}]
-	},
-	options: {
-		scales: {
-			xAxes: [{
-				afterBuildTicks: function(scale, ticks) {
-					var majorUnit = scale._majorUnit;
-					var firstTick = ticks[0];
-					var i, ilen, val, tick, currMajor, lastMajor;
+	var ctx = document.getElementById('chart').getContext('2d');
+	ctx.canvas.width = 1000;
+	ctx.canvas.height = 600;
+	data = null;
 
-					val = luxon.DateTime.fromMillis(ticks[0].value);
-					if ((majorUnit === 'minute' && val.second === 0)
-							|| (majorUnit === 'hour' && val.minute === 0)
-							|| (majorUnit === 'day' && val.hour === 9)
-							|| (majorUnit === 'month' && val.day <= 3 && val.weekday === 1)
-							|| (majorUnit === 'year' && val.month === 0)) {
-						firstTick.major = true;
-					} else {
-						firstTick.major = false;
-					}
-					lastMajor = val.get(majorUnit);
-
-					for (i = 1, ilen = ticks.length; i < ilen; i++) {
-						tick = ticks[i];
-						val = luxon.DateTime.fromMillis(tick.value);
-						currMajor = val.get(majorUnit);
-						tick.major = currMajor !== lastMajor;
-						lastMajor = currMajor;
-					}
-					return ticks;
+	$.ajax({
+		url: "https://api.kraken.com/0/public/OHLC",
+		type: "POST",
+		data: {
+			pair: "XXBTZEUR",
+			interval: 15
+		},
+		// crossOrigin: true,
+		dataType: "json",
+	}).done(function(data){
+		console.log(data);
+		$.each(data.result.XXBTZEUR, function(index, item){
+			// console.log(item);
+			formattedData.push(
+				{
+					t : item[0],
+					o : item[1],
+					h : item[2],
+					l : item[3],
+					c : item[4]
 				}
-			}]
-		}
-	}
-});
+			);
+		})
+		var filteredData = _.filter(formattedData, function(item, index){ if(index % 1 === 0) return item; });
+		console.log(formattedData);
 
-var getRandomInt = function(max) {
-	return Math.floor(Math.random() * Math.floor(max));
-};
+		var chart = new Chart(ctx, {
+			type: 'candlestick',
+			data: {
+				datasets: [{
+					label: 'CHRT - Chart.js Corporation',
+					data: filteredData
+				}]
+			},
+			options: {
+		    plugins: {
+		      crosshair: {
+		        sync: {
+		          enabled: true,          
+		        }
+		      }
+		    },
+				scales: {
+					xAxes: [{
+						type: 'time',
+            distribution: 'series',
+						afterBuildTicks: function(scale, ticks) {
+							// console.log(scale._unit);
+							var majorUnit = scale._unit;
+							var firstTick = ticks[0];
+							var i, ilen, val, tick, currMajor, lastMajor;
 
-function randomNumber(min, max) {
-	return Math.random() * (max - min) + min;
-}
+							val = luxon.DateTime.fromMillis(ticks[0].value);
+							if ((majorUnit === 'minute' && val.second === 0)
+									|| (majorUnit === 'hour' && val.minute === 0)
+									|| (majorUnit === 'day' && val.hour === 9)
+									|| (majorUnit === 'month' && val.day <= 3 && val.weekday === 1)
+									|| (majorUnit === 'year' && val.month === 0)) {
+								firstTick.major = true;
+							} else {
+								firstTick.major = false;
+							}
+							lastMajor = val.get(majorUnit);
 
-function randomBar(date, lastClose) {
-	var open = randomNumber(lastClose * 0.95, lastClose * 1.05).toFixed(2);
-	var close = randomNumber(open * 0.95, open * 1.05).toFixed(2);
-	var high = randomNumber(Math.max(open, close), Math.max(open, close) * 1.1).toFixed(2);
-	var low = randomNumber(Math.min(open, close) * 0.9, Math.min(open, close)).toFixed(2);
-	return {
-		t: date.valueOf(),
-		o: open,
-		h: high,
-		l: low,
-		c: close
-	};
-
-}
-
-function getRandomData(dateStr, count) {
-	var date = luxon.DateTime.fromRFC2822(dateStr);
-	var data = [randomBar(date, 30)];
-	while (data.length < count) {
-		date = date.plus({days: 1});
-		if (date.weekday <= 5) {
-			data.push(randomBar(date, data[data.length - 1].c));
-		}
-	}
-	return data;
-}
-
-var update = function() {
-	var dataset = chart.config.data.datasets[0];
-
-	// candlestick vs ohlc
-	var type = document.getElementById('type').value;
-	dataset.type = type;
-
-	// color
-	var colorScheme = document.getElementById('color-scheme').value;
-	if (colorScheme === 'neon') {
-		dataset.color = {
-			up: '#01ff01',
-			down: '#fe0000',
-			unchanged: '#999',
-		};
-	} else {
-		delete dataset.color;
-	}
-
-	// border
-	var border = document.getElementById('border').value;
-	var defaultOpts = Chart.defaults.global.elements[type];
-	if (border === 'true') {
-		dataset.borderColor = defaultOpts.borderColor;
-	} else {
-		dataset.borderColor = {
-			up: defaultOpts.color.up,
-			down: defaultOpts.color.down,
-			unchanged: defaultOpts.color.up
-		};
-	}
-
-	chart.update();
-};
-
-document.getElementById('update').addEventListener('click', update);
-
-document.getElementById('randomizeData').addEventListener('click', function() {
-	chart.data.datasets.forEach(function(dataset) {
-		dataset.data = getRandomData(initialDateStr, barCount + getRandomInt(10));
+							for (i = 1, ilen = ticks.length; i < ilen; i=i+60) {
+								tick = ticks[i];
+								val = luxon.DateTime.fromMillis(tick.value);
+								currMajor = val.get(majorUnit);
+								tick.major = currMajor !== lastMajor;
+								lastMajor = currMajor;
+							}
+							return ticks;
+						}
+					}]
+				}
+			}
+		});
 	});
-	update();
 });
---
+++ docs/main.js
@@ -0,0 +1,330 @@
+$(function() {
+  formattedData = [];
+  namespace = {
+    defaults: {
+      BASE_URL: "https://api.kraken.com/0/public/",
+      CURRENT_URL: "https://api.kraken.com/0/public/",
+      OHLC_DATA_INTERVAL: 1,
+      PAIR: "XXBTZEUR"
+    },
+
+    init: function(options) {
+      this.setURL(options.url);
+      this.setInterval(options)
+      this.setPair(options)
+
+      return this;
+    },
+
+    fetch: function() {
+      return $.ajax({
+        url: this.getURL(),
+        type: "POST",
+        data: {
+          pair: this.PAIR,
+          interval: this.OHLC_DATA_INTERVAL
+        },
+        dataType: "json",
+      })
+    },
+
+    setURL: function(url) {
+      this.defaults.CURRENT_URL = this.defaults.BASE_URL + url;
+      return this;
+    },
+
+    setInterval: function(options) {
+      this.OHLC_DATA_INTERVAL = (typeof(options.interval) !== "undefined") ? options.interval : this.OHLC_DATA_INTERVAL;
+      return this;
+    },
+
+    setPair: function(options) {
+      this.PAIR = (typeof(options.interval) !== "undefined") ? options.pair : this.PAIR;
+      return this;
+    },
+
+    getURL: function() {
+      return this.defaults.CURRENT_URL;
+    },
+
+    formateData: function(data) {
+      processedData = [];
+      $.each(data.result[this.defaults.PAIR], function(index, item) {
+        // console.log(item);
+        processedData.push({
+          x: item[0] * 1000,
+          y: [item[1], item[2], item[3], item[4]]
+        });
+      })
+
+      return processedData;
+    },
+
+    prepareCandlestickChart: function(ele) {
+      var optionsCandlestick = {
+        chart: {
+          id: 'candles',
+          height: 550,
+          type: 'candlestick',
+          zoom: {
+            type: 'x',
+            enabled: true,
+            autoScaleYaxis: false
+          },
+          animations: {
+            enabled: false,
+            easing: 'linear',
+            dynamicAnimation: {
+              speed: 1000
+            }
+          },
+
+          // toolbar: {
+          //   show: true,
+          //   tools: {
+          //     download: false,
+          //     selection: false,
+          //     zoom: false,
+          //     zoomin: false,
+          //     zoomout: false,
+          //     pan: false,
+          //     reset: false | '<img src="/static/icons/reset.png" width="20">',
+          //     customIcons: []
+          //   }
+          // },
+        },
+        fill: {
+          type: 'pattern',
+          pattern: {
+            style: 'slantedLines',
+            width: 3,
+            height: 3,
+            strokeWidth: 2
+          }
+        },
+
+        annotations: {
+          yaxis: [{
+            y: 6600,
+            borderColor: "#00E396",
+            label: {
+              borderColor: "#00E396",
+              style: {
+                color: "#fff",
+                background: "#00E396"
+              },
+              text: "Y Axis Annotation"
+            }
+          }],
+        },
+        plotOptions: {
+          candlestick: {
+            colors: {
+              upward: '#a6e22e',
+              downward: '#f64f4b'
+            }
+          }
+        },
+        grid: {
+          row: {
+            colors: ['#000']
+          },
+          column: {
+            colors: ['#000']
+          }
+        },
+        series: [{
+          data: formattedData
+        }],
+        stroke: {
+          width: 1
+        },
+        series: [{
+          data: formattedData
+        }],
+        xaxis: {
+          type: 'datetime',
+          labels: {
+            datetimeFormatter: {
+              year: 'yyyy',
+              month: 'MMM \'yy',
+              day: 'dd MMM',
+              hour: 'HH:mm'
+            }
+          },
+          tooltip: {
+            enabled: true
+          },
+          labels: {
+            show: true
+          },
+          tooltip: {
+            enabled: true,
+            formatter: function(value) {
+              var date = new Date(value);
+              var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+              return date.getDate() + " " + months[date.getMonth()] + " " + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes();
+            },
+          },
+        },
+        yaxis: {
+          labels: {
+            show: true
+          },
+          tooltip: {
+            enabled: true,
+            offsetX: -30,
+            formatter: function(value) {
+              return value;
+            }
+          }
+        }
+      }
+
+      return new ApexCharts(
+        document.querySelector(ele),
+        optionsCandlestick
+      );
+    },
+
+    prepareChart: function(ele) {
+      var options = {
+        chart: {
+          height: 160,
+          type: 'candlestick',
+          brush: {
+            enabled: true,
+            target: 'candles'
+          },
+          selection: {
+            enabled: true,
+            xaxis: {
+              min: formattedData[0].x,
+              max: formattedData[50].x
+            },
+            fill: {
+              color: '#ccc',
+              opacity: 0.4
+            },
+            stroke: {
+              color: '#0D47A1',
+            }
+          },
+        },
+        dataLabels: {
+          enabled: false
+        },
+        stroke: {
+          width: 1
+        },
+        series: [{
+          data: formattedData
+        }],
+        xaxis: {
+          type: 'datetime',
+
+        },
+        yaxis: {
+          labels: {
+            show: true
+          }
+        }
+      }
+
+      return new ApexCharts(
+        document.querySelector("#chart-bar"),
+        options
+      );
+    },
+
+    renderChart: function(data) {
+      // console.log(data);
+      formattedData = namespace.formateData(data);
+
+      console.log(formattedData);
+
+      namespace.chartCandlestick = namespace.prepareCandlestickChart('#chart');
+      namespace.chartCandlestick.render();
+
+      window.setInterval(function() {
+        namespace.fetch().done(namespace.updateChart)
+      }, 15000)
+
+      // chartCandlestick.updateSeries([{
+      //   data:
+      // }])
+      // var chart = namespace.prepareChart('#chart-bar');
+      // chart.render();
+    },
+
+    updateChart: function(data) {
+      var data = namespace.formateData(data);
+      namespace.chartCandlestick.updateSeries([{
+        data: data
+      }])
+    }
+  }
+
+  namespace
+    .init({
+      url: "OHLC",
+      interval: 1,
+      pair: "XXBTZEUR"
+    })
+    .fetch()
+    .done(namespace.renderChart);
+
+  $("#1-minute").click(function() {
+    namespace.setInterval({
+      interval: 1
+    }).fetch().done(namespace.updateChart);
+  });
+
+  $("#5-minutes").click(function() {
+    namespace.setInterval({
+      interval: 5
+    }).fetch().done(namespace.updateChart);
+  });
+
+  $("#15-minutes").click(function() {
+    namespace.setInterval({
+      interval: 15
+    }).fetch().done(namespace.updateChart);
+  });
+
+  $("#30-minutes").click(function() {
+    namespace.setInterval({
+      interval: 30
+    }).fetch().done(namespace.updateChart);
+  });
+
+  $("#1-hour").click(function() {
+    namespace.setInterval({
+      interval: 60
+    }).fetch().done(namespace.updateChart);
+  });
+
+  $("#4-hours").click(function() {
+    namespace.setInterval({
+      interval: 240
+    }).fetch().done(namespace.updateChart);
+  });
+
+  $("#1-day").click(function() {
+    namespace.setInterval({
+      interval: 1440
+    }).fetch().done(namespace.updateChart);
+  });
+
+  $("#1-week").click(function() {
+    namespace.setInterval({
+      interval: 10080
+    }).fetch().done(namespace.updateChart);
+  });
+
+  $("#2-weeks").click(function() {
+    namespace.setInterval({
+      interval: 21600
+    }).fetch().done(namespace.updateChart);
+  });
+})
